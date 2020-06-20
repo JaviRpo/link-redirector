a39b5db152d81061d70327a2da9107edd7fb3747
JaviRpo <javier.restrepo.rendon@gmail.com>
2020-06-19 20:19:29 -0500

    Init

---
+++ src/test/java/me/javirpo/linkredirector/LinkRedirectorApplicationTests.java
@@ -0,0 +1,13 @@
+package me.javirpo.linkredirector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkRedirectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
